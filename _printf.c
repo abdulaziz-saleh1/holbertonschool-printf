98219b4fc942ac58cce9507098baa3525e399b27
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-26 23:01:31 +0000

    buffer

--- _printf.c
+++ _printf.c
@@ -1,96 +1,6 @@
 #include "main.h"
 
 /**
-* handle_specifier - Handles format specifiers for _printf
-* @spec: The format specifier
-* @args: List of arguments
-*
-* Return: Number of characters printed
-*/
-int handle_specifier(char spec, va_list args)
-{
-int count = 0;
-
-switch (spec)
-{
-case 'c':
-count += print_char(va_arg(args, int));
-break;
-case 's':
-count += print_string(va_arg(args, char *));
-break;
-case '%':
-count += print_percent();
-break;
-case 'd':
-case 'i':
-count += print_int(va_arg(args, int));
-break;
-case 'b':
-count += print_binary(va_arg(args, unsigned int));
-break;
-case 'u':
-count += print_unsigned_base(va_arg(args, unsigned int), 10, 0);
-break;
-case 'o':
-count += print_unsigned_base(va_arg(args, unsigned int), 8, 0);
-break;
-case 'x':
-count += print_unsigned_base(va_arg(args, unsigned int), 16, 0);
-break;
-case 'X':
-count += print_unsigned_base(va_arg(args, unsigned int), 16, 1);
-break;
-default:
-count += write(1, "%", 1);
-count += write(1, &spec, 1);
-break;
-}
-
-return (count);
-}
-
-/**
-* _printf - Custom printf function
-* @format: Format string
-*
-* Return: Number of characters printed, or -1 on error
-*/
-int _printf(const char *format, ...)
-{
-int i = 0, count = 0;
-va_list args;
-
-if (format == NULL)
-return (-1);
-
-va_start(args, format);
-
-while (format[i])
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '\0')
-{
-va_end(args);
-return (-1);
-}
-i++;
-count += handle_specifier(format[i], args);
-}
-else
-{
-count += write(1, &format[i], 1);
-}
-i++;
-}
-
-va_end(args);
-return (count);
-}
-#include "main.h"
-
-/**
 * flush_buffer - Writes the buffer content to stdout
 * @buffer: The character buffer
 * @len: The number of characters in the buffer
@@ -123,11 +33,11 @@ flush_buffer(buffer, *index);
 }
 
 /**
-* handle_specifier - Processes format specifiers
+* handle_specifier - Handles format specifiers for _printf
 * @spec: Format specifier
 * @args: Arguments list
-* @buffer: The character buffer
-* @index: Pointer to current buffer index
+* @buffer: Output buffer
+* @index: Current buffer index
 *
 * Return: Number of characters printed
 */
@@ -135,8 +45,7 @@ int handle_specifier(char spec, va_list args, char *buffer, int *index)
 {
 int count = 0;
 char *str;
-char temp[32];
-int i, len;
+int i;
 
 switch (spec)
 {
@@ -145,7 +54,8 @@ buffer_char(buffer, va_arg(args, int), index, &count);
 break;
 case 's':
 str = va_arg(args, char *);
-if (!str) str = "(null)";
+if (!str)
+str = "(null)";
 for (i = 0; str[i]; i++)
 buffer_char(buffer, str[i], index, &count);
 break;
@@ -180,10 +90,10 @@ return (count);
 }
 
 /**
-* _printf - Custom printf function with 1024 buffer
+* _printf - Custom printf function using 1024 char buffer
 * @format: Format string
 *
-* Return: Total number of characters printed
+* Return: Number of characters printed
 */
 int _printf(const char *format, ...)
 {
