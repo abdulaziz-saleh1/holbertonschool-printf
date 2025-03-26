274c89b947be30c5322b9037ad14a0a18fcb0eac
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-26 20:09:30 +0000

    _printf

--- print_char.c
+++ print_char.c
@@ -1,21 +1,5 @@
 #include "main.h"
 
-/**
- * print_char - Prints a single character
- * @c: The character to print
- * Return: Number of characters printed (1)
- */
-int print_char(char c)
-{
-    return (write(1, &c, 1));
-}
-#include "main.h"
-
-/**
- * print_char - Prints a single character
- * @c: The character to print
- * Return: Number of characters printed (1)
- */
 int print_char(char c)
 {
     return (write(1, &c, 1));
