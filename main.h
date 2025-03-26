9ac9cff6d3fa8870f1adfd9fd0b4d529942fc287
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-03-26 23:02:51 +0000

    buffer

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(const char *str);
@@ -14,7 +15,7 @@ int print_unsigned_base(unsigned int n, int base, int uppercase);
 int print_int_buffer(int n, char *buffer, int *index);
 int print_binary_buffer(unsigned int n, char *buffer, int *index);
 int print_unsigned_buffer(unsigned int n, int base, int uppercase,
-			  char *buffer, int *index);
+                          char *buffer, int *index);
 int handle_specifier(char spec, va_list args, char *buffer, int *index);
 
-#endif
+#endif 
